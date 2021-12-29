41345fadcee5c298f2064698e341e6fe60a34beb
carlos perez <cpere123@cnm.edu>
2021-12-28 17:21:15 -0700

    added to index.html file

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function setColorPickerEventListeners (colorBox, rgb) {
 
 function setBoxBGColor (colorBox, red, green, blue) {
     rgbVal = [red, green, blue].join(',');
-    colorBox.style.backgroundColor = "rgb(" + rgbVal + ")";
+    colorBox.style.backgroundColor = "rgb(" + rgbVal + ")"
 
 }
 
