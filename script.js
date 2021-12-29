97f361b1aa3541dca4e5c5274db7cf203a9b31b2
carlos perez <cpere123@cnm.edu>
2021-12-28 17:28:23 -0700

    added to index.html file

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ window.onload = function(){
 };
 
 function initColorPicker(){
-
-    let colorBox = document.getElementById('colorBox')
+    let colorBox = document.getElementById("colorBox");
     let rgb = {
         red: document.getElementById("red"),
         green: document.getElementById("green"),
@@ -15,14 +14,14 @@ function initColorPicker(){
 
 function setColorPickerEventListeners (colorBox, rgb) {
     rgb.red.addEventListener('change', () => {
-        console.log("Red Value: ", rgb.red.value);
+        console.log("Red value: ", rgb.red.value);
         setBoxBGColor(colorBox, 150, 150, 150);
     });
     rgb.green.addEventListener('change', () => {
-        console.log("Green Value: ", rgb.green.value);
+        console.log("Green value: ", rgb.green.value);
     });
     rgb.blue.addEventListener('change', () => {
-        console.log("Blue Value: ", rgb.blue.value);
+        console.log("Blue value: ", rgb.blue.value);
     });
 
 }
