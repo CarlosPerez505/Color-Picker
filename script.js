586031128426b4182b69be5436fad10939605b62
carlos perez <cpere123@cnm.edu>
2022-01-06 10:46:55 -0700

    added to js file

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.onload = function(){
 
 
 function initColorPicker(){
-        let colorBox = document.getElementById("colorBox")
+        let colorBox = document.getElementById("color-box")
         let rgb = {
         red: document.getElementById("red"),
         green: document.getElementById("green"),
