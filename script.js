1313b5fa066903bf3e1180c4a018ad06131ce514
carlos perez <cpere123@cnm.edu>
2022-01-06 15:24:31 -0700

    added to js file

--- script.js
+++ script.js
@@ -16,13 +16,15 @@ function initColorPicker(){
 function setColorPickerEventListeners (colorBox, rgb) {
     rgb.red.addEventListener('change', () => {
         console.log("Red value: ", rgb.red.value);
-        setBoxBGColor(colorBox, 150, 150, 150);
+        setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
     });
     rgb.green.addEventListener('change', () => {
         console.log("Green value: ", rgb.green.value);
+        setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
     });
     rgb.blue.addEventListener('change', () => {
         console.log("Blue value: ", rgb.blue.value);
+        setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
     });
 
 }
